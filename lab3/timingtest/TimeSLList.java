465075565318bb203457255660a349ee1d33b53b
smallgame <211098237@smail.nju.edu.cn>
2023-01-11 17:48:16 +0800

    retry

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,4 +1,5 @@
 package timingtest;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -22,7 +23,32 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        int M=10000;
+        Integer [] l={1000,2000,4000,8000,16000,32000,64000,128000};
+        AList<Integer> Ns=new AList<>();
+        AList<Integer> op=new AList<>();
+        for(int i=0;i<l.length;i++)
+        {
+            Ns.addLast(l[i]);
+            op.addLast(M);
+        }
+        AList<Double> time=new AList<>();
+        for(int i=0;i<l.length;i++)
+        {
+            SLList<Integer> s=new SLList<>();
+            for(int j=0;j<l[i];j++)
+            {
+                s.addLast(0);
+            }
+            Stopwatch sw=new Stopwatch();
+            for(int j=0;j<M;j++)
+            {
+                s.getLast();
+            }
+            double tim= sw.elapsedTime();
+            time.addLast(tim);
+        }
+        printTimingTable(Ns,time,op);
     }
 
 }
